a9c7ddd1773dd3431a9171556623a5862341be36
500ping <ndt512x@gmail.com>
2021-07-01 21:31:04 +0700

    show message

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,26 +1,61 @@
 const socket = io()
 
+// Elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+
+const $sendLocationButton = document.querySelector('#send-location')
+
+const $messages = document.querySelector('#messages')
+
+// Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML
+
 socket.on('message', (message) => {
     console.log(message)
+
+    const html = Mustache.render(messageTemplate, {
+        message
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
 })
 
-document.querySelector('#message-form').addEventListener('submit', (event) => {
+$messageForm.addEventListener('submit', (event) => {
     event.preventDefault()
+
+    $messageFormButton.setAttribute('disabled', 'disabled')
 
     const message = event.target.elements.message.value
 
-    socket.emit('sendMessage', message)
+    socket.emit('sendMessage', message, (error) => {
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
+        if (error) {
+            return console.log(error)
+        }
+
+        console.log('Message Delivered!')
+    })
 })
 
-document.querySelector('#send-location').addEventListener('click', () => {
+$sendLocationButton.addEventListener('click', () => {
     if (!navigator.geolocation) {
         return alert('Geolocation  is not supported by your browser.')
     }
 
+    $sendLocationButton.setAttribute('disabled', 'disabled')
+
     navigator.geolocation.getCurrentPosition((position) => {
+        $sendLocationButton.removeAttribute('disabled')
+
         socket.emit('sendLocation', {
             lat: position.coords.latitude,
             lon: position.coords.longitude
+        }, () => {
+            console.log('Location shared!')
         })
     })
 })
